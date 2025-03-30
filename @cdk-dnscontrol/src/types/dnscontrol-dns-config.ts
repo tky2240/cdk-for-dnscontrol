9adc29ffb2997aa7dcf460d835602f724e74b2c0
tky2240 <67401758+tky2240@users.noreply.github.com>
2025-03-30 16:46:48 +0900

    rename dns config keys (#7)
    * rename dns config keys
    
    * format

--- @cdk-dnscontrol/src/types/dnscontrol-dns-config.ts
+++ @cdk-dnscontrol/src/types/dnscontrol-dns-config.ts
@@ -4,6 +4,6 @@ import { DnscontrolRegistrarConfig } from "./dnscontrol-registrar-config";
 
 export type DnscontrolDnsConfig = {
   registrars: DnscontrolRegistrarConfig[];
-  dns_providers: DnscontrolDnsProviderConfig[];
+  dnsProviders: DnscontrolDnsProviderConfig[];
   domains: DnscontrolDomainConfig[];
 };
